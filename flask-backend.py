415943ab991e928031e1e03f435c903b43cc6516
a-dubs <alec.warren@canonical.com>
2024-08-05 12:00:24 -0400

    feat(backend): handle no existing payload better and cap max no of jobs at 30

--- flask-backend.py
+++ flask-backend.py
@@ -5,6 +5,7 @@
 
 from flask import Flask, request, jsonify
 import json
+from datetime import datetime
 import subprocess
 
 app = Flask(__name__)
@@ -23,17 +24,31 @@ def merge_lists_of_dicts(list1, list2):
 
     return merged_list
 
+def get_latest_datetime_str(dates: list[str]) -> datetime:
+    datetimes = [datetime.strptime(date.rsplit(",", maxsplit=1)[0], "%b %d, %Y") for date in dates]
+    # sort the datetimes in descending order
+    datetimes.sort(reverse=True)
+    return datetimes[0]
+
+
 
 @app.route("/payload", methods=["POST"])
 def payload():
     payload = request.get_json()
-    with open("payload.json", "r") as f:
-        existing_payload = json.load(f)
-    print("existing payload length:", len(existing_payload))
-    merged_payload = merge_lists_of_dicts(existing_payload, payload)
-    print("merged payload length:", len(merged_payload))
-    with open("payload.json", "w") as f:
-        json.dump(merged_payload, f)
+    try:
+        with open("payload.json", "r") as f:
+            existing_payload = json.load(f)
+    # if file not found or json decode error
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print("Existing payload not found. Creating new payload file.")
+        with open("payload.json", "w") as f:
+            json.dump(payload, f)
+    else:
+        print("existing payload length:", len(existing_payload))
+        merged_payload = merge_lists_of_dicts(existing_payload, payload)
+        print("merged payload length:", len(merged_payload))
+        with open("payload.json", "w") as f:
+            json.dump(merged_payload, f)
     # generate the html tables
     subprocess.run(["python", "generate-html.py"])
     return jsonify({"message": "Payload received and merged successfully!"}), 200
@@ -42,17 +57,22 @@ def payload():
 # endpoint that returns the latest date of the payload data
 @app.route("/num-jobs-to-fetch", methods=["GET"])
 def num_jobs_to_fetch():
-    with open("payload.json", "r") as f:
-        data = json.load(f)
-    # get the "datetime" key from all the items in the payload and keep the latest date
-    # in the format of "May 5, 2024, 6:41:10 PM"
-    latest_job = max([str(item["datetime"]).rsplit(",", maxsplit=1)[0] for item in data])
-    import datetime
+    try:
+        with open("payload.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError as e:
+        number_of_days = 30
+    else:
+        # get the "datetime" key from all the items in the payload and keep the latest date
+        # in the format of "May 5, 2024, 6:41:10 PM"
+        # latest_job = max([str(item["datetime"]).rsplit(",", maxsplit=1)[0] for item in data])
 
-    latest_job_datetime = datetime.datetime.strptime(latest_job, "%B %d, %Y")
-    # get number of days since the latest date and then add 1
-    # to get the number of jobs to fetch
-    number_of_days = (datetime.datetime.now() - latest_job_datetime).days + 1
+        # latest_job_datetime = datetime.strptime(latest_job, "%B %d, %Y")
+        latest_job_datetime = get_latest_datetime_str([str(item["datetime"]) for item in data])
+        print("Last job datetime:", latest_job_datetime)
+        # get number of days since the latest date and then add 1
+        # to get the number of jobs to fetch
+        number_of_days = min((datetime.now() - latest_job_datetime).days + 1, 30)
     print("number of days:", number_of_days)
     return jsonify({"num_jobs_to_fetch": number_of_days}), 200
 
